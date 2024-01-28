0fe19005645f1edbb8531f87067b0b7331fd5ca9
devnetworking <mizouden@gmail.com>
2024-01-27 21:16:16 -0500

    refaire le fichier server.py

--- honeypot/server.py
+++ honeypot/server.py
@@ -1,15 +1,36 @@
 import socket
+import threading
+import signal
 
-def start_honeypot(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', port))
-    s.listen(5)
-    print(f"Honeypot running on port {port}")
+class Honeypot:
+    def __init__(self, port):
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.running = False
+        self.thread = None
 
-    try:
-        while True:
-            client_socket, addr = s.accept()
-            print(f"Connection from {addr} has been established.")
-            client_socket.close()
-    except KeyboardInterrupt:
-        s.close()
+    def start(self):
+        self.socket.bind(('0.0.0.0', self.port))
+        self.socket.listen(5)
+        print(f"Honeypot running on port {self.port}")
+        self.running = True
+        self.thread = threading.Thread(target=self._listen)
+        self.thread.start()
+
+    def _listen(self):
+        try:
+            while self.running:
+                client_socket, addr = self.socket.accept()
+                print(f"Connection from {addr} has been established.")
+                client_socket.close()
+        finally:
+            self.socket.close()
+
+    def stop(self):
+        print("Stopping Honeypot...")
+        self.running = False
+        self.socket.close()
+        self.thread.join()
+
+
+
