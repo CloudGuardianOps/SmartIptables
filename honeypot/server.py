2c65e5afbda81db7acbbe2c7d243bc4462c8d857
devnetworking <mizouden@gmail.com>
2024-01-27 21:48:47 -0500

    Dev

--- honeypot/server.py
+++ honeypot/server.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-import signal
 
 class Honeypot:
     def __init__(self, port):
@@ -10,27 +9,30 @@ class Honeypot:
         self.thread = None
 
     def start(self):
-        self.socket.bind(('0.0.0.0', self.port))
-        self.socket.listen(5)
-        print(f"Honeypot running on port {self.port}")
-        self.running = True
-        self.thread = threading.Thread(target=self._listen)
-        self.thread.start()
+        try:
+            self.socket.bind(('0.0.0.0', self.port))
+            self.socket.listen(5)
+            print(f"Honeypot running on port {self.port}")
+            self.running = True
+            self.thread = threading.Thread(target=self._listen)
+            self.thread.daemon = True  # Configure le thread en mode daemon
+            self.thread.start()
+        except Exception as e:
+            print(f"Erreur lors du démarrage du honeypot : {e}")
 
     def _listen(self):
-        try:
-            while self.running:
+        while self.running:
+            try:
                 client_socket, addr = self.socket.accept()
                 print(f"Connection from {addr} has been established.")
                 client_socket.close()
-        finally:
-            self.socket.close()
+            except socket.error:
+                break  # Arrête la boucle si le socket est fermé
 
     def stop(self):
-        print("Stopping Honeypot...")
-        self.running = False
-        self.socket.close()
-        self.thread.join()
-
-
-
+        if self.running:
+            print("Stopping Honeypot...")
+            self.running = False
+            self.socket.close()
+            self.thread.join()
+            print("Honeypot stopped.")
